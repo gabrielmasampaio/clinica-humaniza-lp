803bf4ea441bf250fe80a7f1c3a40f555fbd30c8
Gabriel Sampaio <gabrielmasampaio@gmail.com>
2023-11-20 23:41:05 -0300

    Change logo placement

--- frontend/components/sections/schedule.js
+++ frontend/components/sections/schedule.js
@@ -13,18 +13,14 @@ const DynamicVideoPlayer = dynamic(() => import('../media/videoPlayer'), {
 export default function Schedule(){
 
   return (
-      <div className="flex flex-col justify-between min-h-[100vh] ">
-        <div>
-          <Image className="max-w-[10vw]" src='/images/white-text-logo.png' width="auto" height="auto" alt="logo"  />
-        </div>
-        <div className="flex flex-col h-[75%] w-full justify-center items-center">
-          <div className="w-full flex flex-grow flex-row items-center">
-            <div className="flex w-1/2 justify-center">
-              <DynamicVideoPlayer />
-            </div>
-            <div className="flex w-1/2 justify-center items-center">
-              <ScheduleForm />
-            </div>
+      <div className="flex flex-col justify-between min-h-[100vh] pt-20">
+        <div className="w-full flex flex-grow flex-row h-[75%] justify-center ">
+          <div className="flex w-1/2 justify-center">
+            <DynamicVideoPlayer />
+          </div>
+          <div className="flex flex-col w-1/2 min-h-full items-center">
+            <Image className="max-w-[20vw]" src='/images/white-text-logo.png' width="auto" height="auto" alt="logo"  />
+            <ScheduleForm />
           </div>
         </div>
         <div className="flex flex-col justify-center items-center">
