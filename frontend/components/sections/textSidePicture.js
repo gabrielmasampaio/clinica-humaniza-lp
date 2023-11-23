9239be8a933a62eb321bd8e3260ebd8613d25def
Gabriel Sampaio <gabrielmasampaio@gmail.com>
2023-11-23 01:46:20 -0300

    Add isVertical handling to last page

--- frontend/components/sections/textSidePicture.js
+++ frontend/components/sections/textSidePicture.js
@@ -2,8 +2,11 @@ import TextCard from "../media/textCard";
 import {Image} from "@nextui-org/react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faAward} from "@fortawesome/free-solid-svg-icons";
+import {useResponsive} from "../providers/context";
 
 export default function TextSidePicture(){
+  const { isVertical } = useResponsive();
+
   const accoladesBodyText = () => {
     const accolades = "  Graduado em Odontologia pela Universidade de Uberaba;         Especialista em Cirurgia e Traumatologia Bucomaxilofacial;          Especialista em Implantodontia;          Pós Graduação em cirurgia ortognática;          Pós Graduação em cirurgia da ATM;          Pós Graduação em Artroscopia da ATM;          Pós Graduação em Cirurgia Plástica periodontal;          Pós graduado em Harmonização oro facial."
     const accoladesArray = accolades.split(';').map(accolade => accolade.trim());
@@ -28,11 +31,11 @@ export default function TextSidePicture(){
 
   return (
       <div className="flex flex-col min-h-full min-w-full">
-        <div className="min-w-full min-h-full flex justify-end items-center">
+        <div className={"min-w-full min-h-full flex justify-end items-center " + ((isVertical) ? " flex-col p-4" : "")}>
           <div className="flex justify-center items-center min-w-[65%]">
             <TextCard bodyText={accoladesBodyText()} footerText={accoladesFooterText()}/>
           </div>
-          <div className="max-w-[35%]">
+          <div className={((isVertical) ? "" : "max-w-[35%] ")}>
             <Image src="/images/final-image.jpg" height="auto" />
           </div>
         </div>
